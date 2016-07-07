7e674b301f2b3c74d4113fa7840fc325d62ce69c
John <johnjchung@gmail.com>
2016-07-07 12:05:44 -0400

    Added show command for carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,10 @@
 class CartsController < ApplicationController
 
+  ##
+  # Show command for cart
+  #
+  def show
+    @cart = Cart.find_by_uuid(params[:cart_id])
+    render json: @cart, status: 200
+  end
 end
